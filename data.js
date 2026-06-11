cb81aaa7d2b803ec352885382e1ffa552dd4b05c
Eyal Buzaglo <eyal2310@gmail.com>
2026-06-11 23:42:38 +0300

    מצגת סוף שנה כיתה ד3 - גרסה ראשונה

---
+++ data.js
@@ -0,0 +1,368 @@
+window.SLIDES = [
+ {
+  "type": "image",
+  "src": "img/2026-05-21_001.jpg",
+  "date": "2026-05-21",
+  "group": "teacher",
+  "caption": "",
+  "faces": 9
+ },
+ {
+  "type": "image",
+  "src": "img/2026-06-02_002.jpg",
+  "date": "2026-06-02",
+  "group": "teacher",
+  "caption": "",
+  "faces": 2
+ },
+ {
+  "type": "image",
+  "src": "img/2026-06-02_003.jpg",
+  "date": "2026-06-02",
+  "group": "teacher",
+  "caption": "",
+  "faces": 6
+ },
+ {
+  "type": "image",
+  "src": "img/2026-06-02_004.jpg",
+  "date": "2026-06-02",
+  "group": "teacher",
+  "caption": "",
+  "faces": 6
+ },
+ {
+  "type": "video",
+  "src": "vid/2026-06-02_01.mp4",
+  "date": "2026-06-02",
+  "group": "teacher",
+  "caption": ""
+ },
+ {
+  "type": "image",
+  "src": "img/2026-06-02_005.jpg",
+  "date": "2026-06-02",
+  "group": "teacher",
+  "caption": "",
+  "faces": 5
+ },
+ {
+  "type": "image",
+  "src": "img/2026-06-02_006.jpg",
+  "date": "2026-06-02",
+  "group": "teacher",
+  "caption": "",
+  "faces": 7
+ },
+ {
+  "type": "image",
+  "src": "img/2026-06-02_007.jpg",
+  "date": "2026-06-02",
+  "group": "teacher",
+  "caption": "",
+  "faces": 6
+ },
+ {
+  "type": "image",
+  "src": "img/2026-06-02_008.jpg",
+  "date": "2026-06-02",
+  "group": "teacher",
+  "caption": "",
+  "faces": 9
+ },
+ {
+  "type": "image",
+  "src": "img/2026-06-02_009.jpg",
+  "date": "2026-06-02",
+  "group": "teacher",
+  "caption": "",
+  "faces": 4
+ },
+ {
+  "type": "image",
+  "src": "img/2026-06-02_010.jpg",
+  "date": "2026-06-02",
+  "group": "teacher",
+  "caption": "",
+  "faces": 5
+ },
+ {
+  "type": "image",
+  "src": "img/2026-06-02_011.jpg",
+  "date": "2026-06-02",
+  "group": "teacher",
+  "caption": "",
+  "faces": 3
+ },
+ {
+  "type": "image",
+  "src": "img/2026-06-02_012.jpg",
+  "date": "2026-06-02",
+  "group": "teacher",
+  "caption": "",
+  "faces": 5
+ },
+ {
+  "type": "image",
+  "src": "img/2026-06-02_013.jpg",
+  "date": "2026-06-02",
+  "group": "teacher",
+  "caption": "",
+  "faces": 3
+ },
+ {
+  "type": "video",
+  "src": "vid/2026-06-02_02.mp4",
+  "date": "2026-06-02",
+  "group": "teacher",
+  "caption": ""
+ },
+ {
+  "type": "image",
+  "src": "img/2026-06-02_014.jpg",
+  "date": "2026-06-02",
+  "group": "teacher",
+  "caption": "",
+  "faces": 3
+ },
+ {
+  "type": "image",
+  "src": "img/2026-06-02_015.jpg",
+  "date": "2026-06-02",
+  "group": "teacher",
+  "caption": "",
+  "faces": 3
+ },
+ {
+  "type": "image",
+  "src": "img/2026-06-02_016.jpg",
+  "date": "2026-06-02",
+  "group": "teacher",
+  "caption": "",
+  "faces": 3
+ },
+ {
+  "type": "image",
+  "src": "img/2026-06-02_017.jpg",
+  "date": "2026-06-02",
+  "group": "teacher",
+  "caption": "",
+  "faces": 3
+ },
+ {
+  "type": "image",
+  "src": "img/2026-06-02_018.jpg",
+  "date": "2026-06-02",
+  "group": "teacher",
+  "caption": "",
+  "faces": 3
+ },
+ {
+  "type": "image",
+  "src": "img/2026-06-02_019.jpg",
+  "date": "2026-06-02",
+  "group": "teacher",
+  "caption": "",
+  "faces": 0
+ },
+ {
+  "type": "image",
+  "src": "img/2026-06-02_020.jpg",
+  "date": "2026-06-02",
+  "group": "teacher",
+  "caption": "",
+  "faces": 6
+ },
+ {
+  "type": "image",
+  "src": "img/2026-06-02_021.jpg",
+  "date": "2026-06-02",
+  "group": "teacher",
+  "caption": "",
+  "faces": 3
+ },
+ {
+  "type": "image",
+  "src": "img/2026-06-02_022.jpg",
+  "date": "2026-06-02",
+  "group": "teacher",
+  "caption": "",
+  "faces": 5
+ },
+ {
+  "type": "image",
+  "src": "img/2026-06-02_023.jpg",
+  "date": "2026-06-02",
+  "group": "teacher",
+  "caption": "",
+  "faces": 0
+ },
+ {
+  "type": "image",
+  "src": "img/2026-06-02_024.jpg",
+  "date": "2026-06-02",
+  "group": "teacher",
+  "caption": "",
+  "faces": 0
+ },
+ {
+  "type": "image",
+  "src": "img/2026-06-02_025.jpg",
+  "date": "2026-06-02",
+  "group": "teacher",
+  "caption": "",
+  "faces": 4
+ },
+ {
+  "type": "image",
+  "src": "img/2026-06-02_026.jpg",
+  "date": "2026-06-02",
+  "group": "teacher",
+  "caption": "",
+  "faces": 1
+ },
+ {
+  "type": "image",
+  "src": "img/2026-06-02_027.jpg",
+  "date": "2026-06-02",
+  "group": "teacher",
+  "caption": "",
+  "faces": 0
+ },
+ {
+  "type": "image",
+  "src": "img/2026-06-02_028.jpg",
+  "date": "2026-06-02",
+  "group": "teacher",
+  "caption": "",
+  "faces": 2
+ },
+ {
+  "type": "image",
+  "src": "img/2026-06-02_029.jpg",
+  "date": "2026-06-02",
+  "group": "teacher",
+  "caption": "",
+  "faces": 2
+ },
+ {
+  "type": "image",
+  "src": "img/2026-06-02_030.jpg",
+  "date": "2026-06-02",
+  "group": "teacher",
+  "caption": "",
+  "faces": 1
+ },
+ {
+  "type": "image",
+  "src": "img/2026-06-02_031.jpg",
+  "date": "2026-06-02",
+  "group": "teacher",
+  "caption": "",
+  "faces": 5
+ },
+ {
+  "type": "image",
+  "src": "img/2026-06-02_032.jpg",
+  "date": "2026-06-02",
+  "group": "teacher",
+  "caption": "",
+  "faces": 2
+ },
+ {
+  "type": "image",
+  "src": "img/2026-06-02_033.jpg",
+  "date": "2026-06-02",
+  "group": "teacher",
+  "caption": "",
+  "faces": 4
+ },
+ {
+  "type": "image",
+  "src": "img/2026-06-02_034.jpg",
+  "date": "2026-06-02",
+  "group": "teacher",
+  "caption": "",
+  "faces": 2
+ },
+ {
+  "type": "image",
+  "src": "img/2026-06-02_035.jpg",
+  "date": "2026-06-02",
+  "group": "teacher",
+  "caption": "",
+  "faces": 3
+ },
+ {
+  "type": "image",
+  "src": "img/2026-06-02_036.jpg",
+  "date": "2026-06-02",
+  "group": "teacher",
+  "caption": "",
+  "faces": 1
+ },
+ {
+  "type": "image",
+  "src": "img/2026-06-02_037.jpg",
+  "date": "2026-06-02",
+  "group": "teacher",
+  "caption": "",
+  "faces": 2
+ },
+ {
+  "type": "image",
+  "src": "img/2026-06-02_038.jpg",
+  "date": "2026-06-02",
+  "group": "teacher",
+  "caption": "",
+  "faces": 2
+ },
+ {
+  "type": "image",
+  "src": "img/2026-06-02_039.jpg",
+  "date": "2026-06-02",
+  "group": "teacher",
+  "caption": "",
+  "faces": 4
+ },
+ {
+  "type": "image",
+  "src": "img/2026-06-02_040.jpg",
+  "date": "2026-06-02",
+  "group": "teacher",
+  "caption": "",
+  "faces": 5
+ },
+ {
+  "type": "image",
+  "src": "img/2026-06-02_041.jpg",
+  "date": "2026-06-02",
+  "group": "teacher",
+  "caption": "",
+  "faces": 5
+ },
+ {
+  "type": "image",
+  "src": "img/2026-06-02_042.jpg",
+  "date": "2026-06-02",
+  "group": "teacher",
+  "caption": "",
+  "faces": 3
+ },
+ {
+  "type": "image",
+  "src": "img/2026-06-02_043.jpg",
+  "date": "2026-06-02",
+  "group": "teacher",
+  "caption": "",
+  "faces": 2
+ },
+ {
+  "type": "image",
+  "src": "img/2026-06-02_044.jpg",
+  "date": "2026-06-02",
+  "group": "teacher",
+  "caption": "",
+  "faces": 4
+ }
+];
